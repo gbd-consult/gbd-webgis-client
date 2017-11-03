cb2ce2c2d5f23cb84edafbc67e1034f6f7f58f36
gebrkn <gebrkn@gmail.com>
2017-11-03 02:40:31 +0100

    position plugin fixed

--- src/plugins/position/index.js
+++ src/plugins/position/index.js
@@ -15,8 +15,14 @@ import ol from 'ol-all';
 
 class Plugin extends app.Plugin {
     init() {
+        app.set({
+            mapMouseXY: app.map().getView().getCenter()
+        })
+
         app.map().on('pointermove', (evt) => {
-            app.set({mapMouseXY: app.map().getCoordinateFromPixel(evt.pixel)})
+            app.set({
+                mapMouseXY: app.map().getCoordinateFromPixel(evt.pixel)
+            })
         });
     }
 }
@@ -27,8 +33,8 @@ class Control extends React.Component {
         let xy = this.props.mapMouseXY || [0, 0];
         return (
             <div>
-                <TextField style={{width: 80}} value={xy[0].toFixed(0)}/>
-                <TextField style={{width: 80}} value={xy[1].toFixed(0)}/>
+                <TextField id="positionX" style={{width: 80}} value={xy[0].toFixed(0)}/>
+                <TextField id="positionY" style={{width: 80}} value={xy[1].toFixed(0)}/>
             </div>
         );
     }
